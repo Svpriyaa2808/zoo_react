852268b30f3a90d7f86646347f2a4340e57a545a
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-04-16 11:31:26 +0200

    adding media query for hamburgermenu

--- src/components/Navigation/DesktopNavigation/index.jsx
+++ src/components/Navigation/DesktopNavigation/index.jsx
@@ -4,8 +4,10 @@ import NavItem from '../NavItem'
 const DesktopNavigation = ({hideAnimalContent}) => {
     return (
         <>
+        <div className={styles.desktop_menu}>
             <NavItem name="Home" />
             {Categories.map((item,index) => <NavItem key={index} name={item} welcomeMessage={hideAnimalContent}/>)}
+        </div>
         </>
     )
 }
--- src/components/Navigation/MobileNavigation/index.jsx
+++ src/components/Navigation/MobileNavigation/index.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react'
 import styles from '../../Navigation/navigation.module.css'
 import MenuIcon from '../../../assets/menu_icon.svg'
+import { Categories } from '../../../data/data'
+import NavItem from '../NavItem'
+
 const MobileNavigation = () => {
+
+    const [showNav,setShowNav] = useState("inactive") //show menu when hamburger is clicked
+
+    const handleMenu = () => {
+        setShowNav(showNav === "active" ? "inactive" : "active")
+    }
+
+
     return (
         <>
-        <div className={styles.hamburger_menu}>
-            <img src={MenuIcon} alt='Menu_icon'></img>
+        <div className={styles.hamburger_menu}  >
+            <img src={MenuIcon} alt='Menu_icon' onClick={handleMenu}></img> 
+
+            <div className={`${styles.mobile_menu} ${styles[showNav]}`}>
+            <NavItem name="Home" />
+            {Categories.map((item,index) => <NavItem key={index} name={item} />)}
+            </div>           
         </div>
     </>
     )
