5458ca1105f2638839481215a6aa2ce85387fcc9
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-04-20 19:41:46 +0200

    adding images and aligning contents in fulldetails

--- src/pages/AnimalDetails/index.jsx
+++ src/pages/AnimalDetails/index.jsx
@@ -19,25 +19,32 @@ const AnimalDetails = () => {
 
     return (
         <div className={styles.animal_details}>
-        <motion.div className={styles.details_container}>
-            {AnimalDisplayed.map((item,index)=>
-            <div key={index} className={styles.full_details_container}>
-                <h2 className={styles.heading}>{item.name}</h2>
-                <img src={getImageUrl(item.image)} alt={item.name}></img>
-                <p className={styles.description}>About {item.name} : {item.description} </p>
-                <p className={styles.details}>Length : {item.length} </p>
-                <p className={styles.details}>Weight : {item.weight} kg </p>
-                <p className={styles.details}>LifeSpan : {item.lifespan} </p>
-                <p className={styles.details}>Food : {item.food} </p>
-                <p className={styles.details}>Found : {item.found} </p>
-            </div>)}
-        
-        </motion.div>
-        
-        <div className={styles.close} onClick={handleClose}>
-        <XCircle size={32} />
-        </div>
-       
+            <motion.div className={styles.details_container}>
+                {AnimalDisplayed.map((item,index)=>
+                <div key={index} className={styles.full_details_container}>
+                    <div className={styles.heading}>
+                        <div className={styles.title}>
+                            <h2>{item.name}</h2>
+                        </div>
+                        <div className={styles.close} onClick={handleClose}>
+                            <XCircle size={38} />
+                        </div>
+                    </div>
+                    <img src={getImageUrl(item.image)} alt={item.name}></img>
+                    <div className={styles.description}>
+                        <h4>About {item.name}</h4>
+                        <p> {item.description} </p>
+                    </div>
+                    <div className={styles.details_div}>
+                        <h4>Features</h4>
+                        <p className={styles.details}>Length : {item.length} cm</p>
+                        <p className={styles.details}>Weight : {item.weight} kg </p>
+                        <p className={styles.details}>LifeSpan : {item.lifespan} years</p>
+                        <p className={styles.details}>Food : {item.food}</p>
+                        <p className={styles.details}>Found : {item.found} </p>
+                    </div>
+                </div>)}
+             </motion.div> 
         </div>
     )
 }
